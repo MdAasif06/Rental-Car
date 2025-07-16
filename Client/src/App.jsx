94eea167a190cbaac6f40248ed1820e95110673b
MdAasif06 <abdurm477@gmail.com>
2025-07-16 11:12:08 +0530

    basic setup is done

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
+
+const App = () => {
+  const [showlogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner")
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+    </>
+  );
+};
+
+export default App;
